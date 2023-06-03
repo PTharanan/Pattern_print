c30a5ba1de99110c1714ddcc7fe7e8d516c172b3
Tharanan <108599220+PTharanan@users.noreply.github.com>
2023-06-03 17:56:21 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : pettern.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int n,i,j;
+	printf("Enter line number: ");
+	scanf("%d",&n);
+	for(i = 0; i < n; i++){
+		for(j = 0; j <= i; j++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
+}
